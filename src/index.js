a86bedc7aee9f74c01a6e245be1399c103b4f2d1
Zakia Afrin Jeme <zjeme@techlaunch.io>
2019-02-26 18:52:37 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import  Avatar  from './Avatar';
+
+
+function Message(){
+    return(
+        <div className="message">
+        This is a message in a tweet.
+        </div>
+    );
+}
+
+function NameWithHandle(){
+    return(
+    <span className="name-with-handle">
+        <span className="name">Your Name</span>
+        <span className="handle">@yourhandle</span>
+    </span>
+    );
+}
+
+const Time = () => (
+    <span className="time">3h ago</span>
+);
+
+const ReplyButton =() =>(
+    <i className="fa fa-reply reply-button"/>
+);
+
+const ReTweetButton =() =>(
+    <i className="fa fa-retweet retweet-button"/>
+);
+
+const LikeButton =() =>(
+    <i className="fa fa-heart retweet-button"/>
+);
+
+const MoreOptionButton =() =>(
+    <i className="fa fa-ellipsis-h more-options-button"/>
+);
+
+
+function Tweet(){
+    return (
+        <div className="tweet">
+            <Avatar/>
+            <div className="content">
+                <NameWithHandle/>
+                <Time/>
+                <Message/>
+                <div className="buttons">
+                    <ReplyButton/>
+                    <ReTweetButton/>
+                    <LikeButton/>
+                    <MoreOptionButton/>
+                </div>
+            </div>
+        </div>
+    )
+}
+ReactDOM.render(<Tweet />, document.getElementById('root'));
+
